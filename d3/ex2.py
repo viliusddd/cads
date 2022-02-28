9bc74b51c17bc281e944a7248eae32b825f8cd01
ViliusJuodziukynas <vilius@vilius.net>
2022-02-28 19:45:22 +0200

    Fix exercise 2

--- d3/ex2.py
+++ d3/ex2.py
@@ -5,7 +5,14 @@
 # įvestų teigiamų skaičių sumą
 # Patarimas: Naudoti ciklą while, sąlygą if, brake
 
+nums = []
+
 while True:
     human_input = int(input("Please insert number: "))
-    if human_input < 0:
+
+    if human_input > 0:
+        nums.append(human_input)
+    else:
         break
+
+print(sum(nums))
